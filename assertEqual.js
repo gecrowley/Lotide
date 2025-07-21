615f627d2ced23bfdd3d8ea5c8cacf6354429d4f
Gavin <gavin.e.crowley@gmail.com>
2025-07-21 14:52:24 -0700

    assert function

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  console.assert(assertEqual);
+  if (actual === expected)
+    console.log("✅✅✅Assertion Passed:" + [actual] + "===" + [expected]);
+  else
+    console.log("❌❌❌Assertion Failed:" + [actual] + "!==" + [expected]);
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
