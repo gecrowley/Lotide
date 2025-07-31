d09211b14d63d9edf0142278e7b3de2a7f005d15
Gavin <gavin.e.crowley@gmail.com>
2025-07-31 14:16:04 -0700

    added without function

---
+++ without.js
@@ -0,0 +1,30 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) 
+    return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) 
+      return false;
+  }
+  return (true);
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log (`✅✅Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log (`❌❌Assertion Failed: ${arr1} !== ${arr2}`);
+}
+}
+
+const without = function (source, itemsToRemove) {
+  const result = [];
+  for (let i = 0; i < source.length; i++) {
+  if (!itemsToRemove.includes(source[i])) {
+      result.push(source[i]);
+  }
+}
+return (result);
+};
+
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
